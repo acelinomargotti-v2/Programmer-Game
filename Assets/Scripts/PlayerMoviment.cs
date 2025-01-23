32114ca3cf8e74af04b53beb310a7ffb1b6bb937
Acelino Margotti <165937963+NewGitGet@users.noreply.github.com>
2025-01-23 17:02:44 -0300

    Added player, environment, and creature.
    The player was created, along with an idea of the creature that the player will later collect and an idea of the environment where both will be. Additionally, a script was added to both the player and the creature.

---
+++ Assets/Scripts/PlayerMoviment.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMoviment : MonoBehaviour
+{
+    public float playerSpeed;
+    public float playerRotationSpeed;
+    public int playerSpaceOnInvetary; //This variable I need to encapsulate 
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
